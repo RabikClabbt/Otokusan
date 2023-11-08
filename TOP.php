43e218450d19884eef686c50b72cbcce0996ba1f
soramame1877 <148169289+soramame1877@users.noreply.github.com>
2023-11-08 11:39:42 +0900

    Top画面
    imageファイルからregionファイルに変更
    
    平井賢太

--- TOP.php
+++ TOP.php
@@ -10,35 +10,35 @@
         <?php
         echo '<div class="region">';
             echo '<div class="ho">';
-                echo '<a href="#hokkaido"><img src="image/Hokkaido.png"></a>';
+                echo '<a href="#hokkaido"><img src="region/Hokkaido.png"></a>';
                 /*echo '<input type="image" name="ho" src="image/Hokkaido.png"  alt="ho" value="ho">';*/
             echo '</div>';
             echo '<div class="to">';
-                echo '<a href="#tohoku"><img src="image/tohoku.png"></a>';
+                echo '<a href="#tohoku"><img src="region/tohoku.png"></a>';
                 /*echo '<input type="image" name="to" src="image/tohoku.png"  alt="to" value="to">';*/
             echo '</div>';
             echo '<div class="kan">';
-                echo '<a href="#kanto"><img src="image/kanto.png"></a>';
+                echo '<a href="#kanto"><img src="region/kanto.png"></a>';
                 /*echo '<input type="image" name="kan" src="image/kanto.png"  alt="kan" value="kan">';*/
             echo '</div>';
             echo '<div class="tyu">';
-                echo '<a href="#tyubu"><img src="image/tyubu.png"></a>';
+                echo '<a href="#tyubu"><img src="region/tyubu.png"></a>';
                 /*echo '<input type="image" name="tyu" src="image/tyubu.png"  alt="tyu" value="tyu">';*/
             echo '</div>';
             echo '<div class="kik">';
-                echo '<a href="#kinki"><img src="image/kinki.png"></a>';
+                echo '<a href="#kinki"><img src="region/kinki.png"></a>';
                 /*echo '<input type="image" name="kik" src="image/kinki.png"  alt="kik" value="kik">';*/
             echo '</div>';
             echo '<div class="tyugo">';
-                echo '<a href="#tyugoku"><img src="image/tyugoku.png"></a>';
+                echo '<a href="#tyugoku"><img src="region/tyugoku.png"></a>';
                 /*echo '<input type="image" name="tyugo" src="image/tyugoku.png"  alt="tyugo" value="tyugo">';*/
             echo '</div>';
             echo '<div class="siko">';
-                echo '<a href="#sikoku"><img src="image/shikoku.png"></a>';
+                echo '<a href="#sikoku"><img src="region/shikoku.png"></a>';
                 /*echo '<input type="image" name="siko" src="image/shikoku.png"  alt="siko" value="siko">';*/
             echo '</div>';
             echo '<div class="kyusy">';
-                echo '<a href="#kyusyu"><img src="image/kyusyu.png"></a>';
+                echo '<a href="#kyusyu"><img src="region/kyusyu.png"></a>';
                 /*echo '<input type="image" name="kyusy" src="image/kyusyu.png"  alt="kyusy" value="kyusy">';*/
             echo '</div>';
         echo '</div>';
