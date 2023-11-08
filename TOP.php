e1d53088bd0eafff4a0c816a7faec16ec4a26105
soramame1877 <148169289+soramame1877@users.noreply.github.com>
2023-11-08 11:20:32 +0900

    Top画面
    ボタンからリンクに変更
    
    平井賢太

--- TOP.php
+++ TOP.php
@@ -1,37 +1,62 @@
 <!DOCTYPE html>
 <html>
-<head>
-<link rel="stylesheet" type="css" href="./css/style.css">
-</head>
-<body>
-    
-<?php
-echo '<div class="region">';
-    echo '<div class="ho">';
-        echo '<input type="image" name="ho" src="image/Hokkaido.png"  alt="ho" value="ho">';
-    echo '</div>';
-    echo '<div class="to">';
-    echo '<input type="image" name="to" src="image/tohoku.png"  alt="to" value="to">';
-    echo '</div>';
-    echo '<div class="kan">';
-    echo '<input type="image" name="kan" src="image/kanto.png"  alt="kan" value="kan">';
-    echo '</div>';
-    echo '<div class="tyu">';
-    echo '<input type="image" name="tyu" src="image/tyubu.png"  alt="tyu" value="tyu">';
-    echo '</div>';
-    echo '<div class="kik">';
-    echo '<input type="image" name="kik" src="image/kinki.png"  alt="kik" value="kik">';
-    echo '</div>';
-    echo '<div class="tyugo">';
-    echo '<input type="image" name="tyugo" src="image/tyugoku.png"  alt="tyu" value="tyu">';
-    echo '</div>';
-    echo '<div class="siko">';
-    echo '<input type="image" name="siko" src="image/shikoku.png"  alt="siko" value="siko">';
-    echo '</div>';
-    echo '<div class="kyusy">';
-    echo '<input type="image" name="kyusy" src="image/kyusyu.png"  alt="kyusy" value="kyusy">';
-    '</div>';
-'</div>';
-?>
-</body>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./css/style.css">
+    </head>
+    <body>
+        <?php
+        echo '<div class="region">';
+            echo '<div class="ho">';
+                echo '<a href="#hokkaido"><img src="image/Hokkaido.png"></a>';
+                /*echo '<input type="image" name="ho" src="image/Hokkaido.png"  alt="ho" value="ho">';*/
+            echo '</div>';
+            echo '<div class="to">';
+                echo '<a href="#tohoku"><img src="image/tohoku.png"></a>';
+                /*echo '<input type="image" name="to" src="image/tohoku.png"  alt="to" value="to">';*/
+            echo '</div>';
+            echo '<div class="kan">';
+                echo '<a href="#kanto"><img src="image/kanto.png"></a>';
+                /*echo '<input type="image" name="kan" src="image/kanto.png"  alt="kan" value="kan">';*/
+            echo '</div>';
+            echo '<div class="tyu">';
+                echo '<a href="#tyubu"><img src="image/tyubu.png"></a>';
+                /*echo '<input type="image" name="tyu" src="image/tyubu.png"  alt="tyu" value="tyu">';*/
+            echo '</div>';
+            echo '<div class="kik">';
+                echo '<a href="#kinki"><img src="image/kinki.png"></a>';
+                /*echo '<input type="image" name="kik" src="image/kinki.png"  alt="kik" value="kik">';*/
+            echo '</div>';
+            echo '<div class="tyugo">';
+                echo '<a href="#tyugoku"><img src="image/tyugoku.png"></a>';
+                /*echo '<input type="image" name="tyugo" src="image/tyugoku.png"  alt="tyugo" value="tyugo">';*/
+            echo '</div>';
+            echo '<div class="siko">';
+                echo '<a href="#sikoku"><img src="image/shikoku.png"></a>';
+                /*echo '<input type="image" name="siko" src="image/shikoku.png"  alt="siko" value="siko">';*/
+            echo '</div>';
+            echo '<div class="kyusy">';
+                echo '<a href="#kyusyu"><img src="image/kyusyu.png"></a>';
+                /*echo '<input type="image" name="kyusy" src="image/kyusyu.png"  alt="kyusy" value="kyusy">';*/
+            echo '</div>';
+        echo '</div>';
+        ?>
+        <h2 id="hokkaido">北海道</h2>
+        <a href= "hokkaido.html" >北海道</a>
+        <h2 id="tohoku">東北地方</h2>
+        <a href= "hokkaido.html" >青森県</a>
+        <a href= "hokkaido.html" >秋田県</a>
+        <a href= "hokkaido.html" >岩手県</a>
+        <a href= "hokkaido.html" >宮城県</a>
+        <a href= "hokkaido.html" >山形県</a>
+        <a href= "hokkaido.html" >福島県</a>
+        <h2 id="kanto">関東地方</h2>
+        <a href= "hokkaido.html" >茨城県</a>
+        <a href= "hokkaido.html" >栃木県</a>
+        <a href= "hokkaido.html" >群馬県</a>
+        <a href= "hokkaido.html" >埼玉県</a>
+        <a href= "hokkaido.html" >東京都</a>
+        <a href= "hokkaido.html" >千葉県</a>
+        <a href= "hokkaido.html" >神奈川県</a>
+
+    </body>
 </html>
