94b46a2d1d5bd5f2858fc32ab64609991c5c5152
Yakko <2201142@s.asojuku.ac.jp>
2023-11-13 12:57:45 +0900

    header及びsidebar
    ヘッダーのUI
    サイドバーのUI
    改良の余地あり
    
    参考HTML（https://RabikClabbt.github.io/Otokusan/test.html）
    
    久保雄太

---
+++ js/Sidebar.js
@@ -0,0 +1,9 @@
+$(".menubtn").click(function () {
+  $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+  $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+  $(".menubtn").removeClass('active');//ボタンの activeクラスを除去し
+  $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
