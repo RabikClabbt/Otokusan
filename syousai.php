0ba50ac9ff8dfcd712ec8bcb97dab50a142e5eff
donnezu <sh20030619@gmail.com>
2023-11-22 15:00:43 +0900

    商品詳細ページ
    画像がでらん！！！！！！！！！
    
    廣永

--- syousai.php
+++ syousai.php
@@ -15,19 +15,18 @@ if (isset($_GET['productID'])) {
     $sql = $pdo->prepare('SELECT * FROM product WHERE productID = ?');
     $sql->execute([$_GET['productID']]);
         foreach ($sql as $row) {
-        echo '<img alt="image" src="image/', $row['imgPass'], '.jpg">]';
+        echo '<img src="image/',$row['imgPass'],'" height="130">';
+        echo '<br>';
         echo '<p>商品名:', $row['productName'], '</p>';
         echo '<p>価格:', $row['price'], '</p>';
 
         echo '<form action="カートページのURL" method="post">';
         echo '<p>個数:<select name="count">';
-        for ($i = 1; $i < 10; $i++) {
+        for ($i = 1; $i < 11; $i++) {
             echo '<option value="' , $i , '">' , $i , '</option>';
         }
         echo '</select></p>';
         echo '<input type="hidden" name="id" value="' . $row['productID'] . '">';
-        echo '<input type="hidden" name="name" value="' . $row['productName'] . '">';
-        echo '<input type="hidden" name="price" value="' . $row['price'] . '">';
         echo '<p><input type="submit" value="カートに追加"></p>';
         echo '</form>';
         echo '<form action="購入画面のURL" method="post">';
