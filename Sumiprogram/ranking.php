9699b10cd138eddefdc3ae5733b5f5c1f0449fe2
su-mi-ii <2201223@s.asojuku.ac.jp>
2023-11-22 14:56:13 +0900

    php
    sumi

--- Sumiprogram/ranking.php
+++ Sumiprogram/ranking.php
@@ -1,3 +1,4 @@
+User
 <?php require 'Header.php';?>
 <?php
 
@@ -16,55 +17,56 @@
     <link rel="stylesheet" href="css/Ranking.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>北海道エリアの商品ページ</title>
-</head>
 
-<body>
-    <div class="header">
-    <div class="area" style="height: 60px; display: flex; flex-direction: column; justify-content: left; align-items: left;">
-
-    北海道エリア　　　　　北海道１
-</div>
-</div>
-    <div class="ad">広告</div>
-    <div class="ranking">
-        売れ筋ランキング👑
-        <ol>
-            <li>商品1👑</li>
-            <li>商品2👑</li>
-            <li>商品3👑</li>
-        </ol>
-    </div>
-
-    <div class="ranking-list">
-        <div class="rproduct">
-            <div class="rproduct-name">商品1👑</div>
-            <img src="image/akakabu.jpg" alt="商品1の写真">
-            <div class="rproduct-description">
-                <h2>商品1</h2>
-            </div>
-        </div>
-
-        <div class="rproduct">
-            <div class="rproduct-name">商品2👑</div>
-            <img src="商品2の画像のURL" alt="商品2の写真">
-            <div class="rproduct-description">
-                <h2>商品2</h2>
-            </div>
-        </div>
-
-        <div class="rproduct">
-            <div class="rproduct-name">商品3👑</div>
-            <img src="商品3の画像のURL" alt="商品3の写真">
-            <div class="rproduct-description">
-                <h2>商品3</h2>
-            </div>
-        </div>
-    </div>
-
-    <div class="ranking">
-        商品一覧
 
 <?php
+
+try {
+    $pdo = new PDO($connect, USER, PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "SELECT p.*, pr.prefecturesName FROM product p
+        JOIN prefectures pr ON p.prefecturesNo = pr.prefecturesNo
+        WHERE p.prefecturesNo = '01'
+        LIMIT 3";
+
+    $stmt = $pdo->query($sql);
+
+    if ($stmt !== false) {
+        $counter = 0; // カウンターを初期化
+
+        foreach ($stmt as $row) {
+            if ($counter % 3 == 0) {
+                // カウンターが4の倍数の場合、新しい行を開始
+                echo '<div class="ranking-list">';
+            }
+
+            echo '<div class="rproduct">';
+            echo '<div class="rproduct-name">' . $row['prefecturesName'] . '</div>';
+            echo '<div class="rproduct img">';
+            echo '<img src="image/' . $row['imgPass'] . '" alt="' . $row['productName'] . '">';
+            echo '</div>';
+            echo '<div class="rproduct-description">';
+            echo '<h2>' . $row['productName'] . '</h2>';
+            echo '<h3>' . $row['price'] . '</h3>';
+            echo '</div>';
+            echo '</div>';
+
+            
+            if (($counter + 1) % 3  === 0) {
+                // カウンター+1が4の倍数の場合、行を閉じる
+                echo '</div>';
+            }
+
+            $counter++;
+        }
+    }
+
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+
+
+}
 try {
     $pdo = new PDO($connect, USER, PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -85,7 +87,7 @@ try {
 
             echo '<div class="rproduct">';
             echo '<div class="rproduct-name">' . $row['prefecturesName'] . '</div>';
-            echo '<img src="' . $row['imgPass'] . '" alt="' . $row['productName'] . '">';
+            echo '<img src="image/' . $row['imgPass'] . '" alt="' . $row['productName'] . '">';
             echo '<div class="rproduct-description">';
             echo '<h2>' . $row['productName'] . '</h2>';
             echo '<h3>' . $row['price'] . '</h3>';
