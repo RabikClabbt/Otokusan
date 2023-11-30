7ef5245e91f3ec1bb58d82a9994e05c70f803abc
Yakko <2201142@s.asojuku.ac.jp>
2023-11-30 10:31:29 +0900

    ゆっくりしていってね

--- Header.php
+++ Header.php
@@ -8,7 +8,7 @@
         </div>
         <div class="search-container">
             <form action="Result.php" type="post"></form>
-            <input type="text" autocomplete="off" aria-autocomplete="list" aria-controls="react-autowhatever-1" class="search-input" placeholder="キーワードを入力" name="sname" spellcheck="false" data-ms-editor="true">
+            <input type="text" autocomplete="off" aria-autocomplete="list" aria-controls="react-autowhatever-1" class="search-input" placeholder="キーワードを入力" name="sname" value="" spellcheck="false" data-ms-editor="true">
             <button type="submit" class="search-button">
                 <img src="./icon/Search.png" width="20" height="20" alt="検索">
             </button>
