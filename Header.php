ab9d40cbe638ba309512ca2ee9e34a79af864949
Yakko <2201142@s.asojuku.ac.jp>
2023-11-30 09:57:50 +0900

    Cart周りの修正
    バックグラウンドPHPにセッションカート要処理を追加
    個数上限を10に設定
    
    久保雄太

--- Header.php
+++ Header.php
@@ -7,16 +7,16 @@
             </a>
         </div>
         <div class="search-container">
-            <form action="検索結果画面へのリンク" type="post"></form>
-            <input type="text" autocomplete="off" aria-autocomplete="list" aria-controls="react-autowhatever-1" class="search-input" placeholder="キーワードを入力" value="" spellcheck="false" data-ms-editor="true">
+            <form action="Result.php" type="post"></form>
+            <input type="text" autocomplete="off" aria-autocomplete="list" aria-controls="react-autowhatever-1" class="search-input" placeholder="キーワードを入力" value="sname" spellcheck="false" data-ms-editor="true">
             <button type="submit" class="search-button">
                 <img src="./icon/Search.png" width="20" height="20" alt="検索">
             </button>
         </div>
     </div>
         <div class="right">
-            <a href="検索画面へのリンク"><img src="./icon/Search.png" width="30" height="30" alt="検索"></a>
-            <a href="カート画面へのリンク"><img src="./icon/Cart.png" width="30" height="30" alt="カート"></a>
+            <a href="Search.php"><img src="./icon/Search.png" width="30" height="30" alt="検索"></a>
+            <a href="Cart.php"><img src="./icon/Cart.png" width="30" height="30" alt="カート"></a>
             <a href="アカウント画面へのリンク"><img src="./icon/Account.png" width="30" height="30" alt="アカウント"></a>
         </div>
 </header>
